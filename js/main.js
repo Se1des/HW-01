08cd1c7b7690400d97001bf621f77cf9955e9dbd
Se1des <115939994+Se1des@users.noreply.github.com>
2024-04-24 18:36:21 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+document.querySelectorAll('.menu__link').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+
+        if (targetSection) {
+            window.scrollTo({
+                top: targetSection.offsetTop,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
